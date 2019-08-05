7d71b2778f2ee43d555305a9f9c12587a5b19da8
Zer0 <lucismcg@gmail.com>
2019-08-05 09:08:46 -0600

    javascript finished and functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,130 @@
 
-var counter = 30;
+var counter = 15;
 var currentQuestion = 0;
 var score = 0;
+var lose = 0;
 var timer;
 
-function loadQuestion() {
-    const question = quizQuestions[currentQuestion].question; //
-    const choices = quizQuestions[currentQuestion].choices; //
-    $("#time").html("time remaining: " + counter);
-    $("#game").html(
-        '<p>' + question + '</p>');
-    $(loadChoices(choices));
+function nextQuestion() {
+
+    var gameOver = (quizQuestions.length - 1) === currentQuestion;
+
+    if (gameOver) {
+        displayResult();
+    }
+
+    else {
+        currentQuestion++;
+        loadQuestion();
+    }
 }
 
-    function loadChoices(choices) {
-        var result = '';
+function timeUp() {
+    clearInterval(timer);
+    lose++;
+    preloadImage("lose");
+    setTimeout(nextQuestion, 4 * 1000);
+}
 
-        for (let index = 0; index < choices.length; index++) {
-            result += `<p class="choice" data-answer="${choices[i]}">${choices[i]}</p>`;
-            
-            return result;
-        }
+function countDown() {
+    counter--;
+    $("#time").html("Time remaining: " + counter);
+
+    if (counter === 0) {
+        timeUp();
     }
-    $("#game").html("<p>crap</p>");
+}
 
+function loadQuestion() {
+
+    counter = 15;
+    timer = setInterval(countDown, 1000);
+
+    const question = quizQuestions[currentQuestion].question; //
+    const choices = quizQuestions[currentQuestion].choices; //
+    $("#time").html("Time remaining: " + counter);
+    $("#game").html(`
+        <h4>${question}</h4>
+    ${loadChoices(choices)}
+    ${loadRemainingQuestions()}
+    `);
+}
+
+function loadChoices(choices) {
+    var result = '';
+
+    for (var i = 0; i < choices.length; i++) {
+        result += `<p class="choice" data-answer="${choices[i]}">${choices[i]}</p>`;
+    }
+    return result;
+
+}
+
+$(document).on("click", ".choice",function () {
+    clearInterval(timer);
+    const selectedAnswer = $(this).attr("data-answer");
+    const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+    
+    if (correctAnswer === selectedAnswer){
+        score++;
+        preloadImage("win");
+        setTimeout(nextQuestion, 4 * 1000);
+    }
+    else {
+            lose++;
+            preloadImage("lose");
+            setTimeout(nextQuestion, 4 * 1000);
+        }
+
+})
+
+function displayResult() {
+    const result = `
+    <p>You got ${score} out of ${quizQuestions.length} questions right</p>
+    <button class="btn btn-primary" id="resetBtn">Restart Game?</button>
+    `;
+    $("#game").html(result);
+}
+
+$(document).on("click","#resetBtn", function(){
+counter = 15;
+currentQuestion = 0;
+score = 0;
+lose = 0;
+timer = null;
 loadQuestion();
+})
+
+
+function loadRemainingQuestions() {
+    const remainingQuestions = quizQuestions.length - (currentQuestion + 1);
+    const totalQuestions = quizQuestions.length;
+    return `Questions Left ${remainingQuestions}/${totalQuestions}`;
+
+}
+
+function randomImage(images) {
+    const random = Math.floor(Math.random() * images.length);
+    const randomImage = images[random];
+    return randomImage;
+}
+
+function preloadImage(status) {
+    const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+    if (status === "win"){
+        $("#game").html
+        (`<p class="preload-image">Congratulations! The Correct answer was <b>${correctAnswer}</b></p>
+        <img src="${randomImage(winImg)}"/>
+        `);}
+    
+    else {
+    $("#game").html
+    (`<p class="preload-image">Too Bad! The Correct answer was <b>${correctAnswer}</b></p>
+    <img src="${randomImage(loseImg)}"/>`);}
+}
+
+$("#start").click(function(){
+    $("#start").remove();
+    $("#time").html(counter);
+    loadQuestion();
+});
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -15,3 +15,27 @@ const quizQuestions = [
         correctAnswer: "green"
     }
     ];    
+
+    const winImg = [
+    "./assets/images/win1.gif", 
+    "./assets/images/win2.gif", 
+    "./assets/images/win3.gif", 
+    "./assets/images/win4.gif", 
+    "./assets/images/win5.gif", 
+    "./assets/images/win6.gif", 
+    "./assets/images/win7.gif", 
+    "./assets/images/win8.gif", 
+    "./assets/images/win9.gif", 
+    "./assets/images/win10.gif",];
+    
+    const loseImg = [
+    "./assets/images/fail1.gif", 
+    "./assets/images/fail2.gif", 
+    "./assets/images/fail3.gif", 
+    "./assets/images/fail4.gif", 
+    "./assets/images/fail5.gif", 
+    "./assets/images/fail6.gif", 
+    "./assets/images/fail7.gif", 
+    "./assets/images/fail8.gif", 
+    "./assets/images/fail9.gif", 
+    "./assets/images/fail10.gif",];
